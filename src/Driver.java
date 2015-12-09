188a4c129b83725371ece15223e3f09d95f30ec2
kjeff1475 <kjeff1475@gmail.com>
2015-12-09 12:11:55 -0500

    updated driver

--- src/Driver.java
+++ src/Driver.java
@@ -1,9 +1,10 @@
+
+
 import java.awt.Point;
 import java.util.ArrayList;
 
 /**
  * The driver for the project
- * @author Julius Phu
  */
 public class Driver
 {
@@ -13,34 +14,96 @@ public class Driver
      */
     public static void main(String[] args)
     {
-        Line[] lines = LineGenerator.generateLines(10, 50);
-//        lines[0] = new Line(9,21, true);
-//        lines[1] = new Line(12,8, true);
-//        lines[2] = new Line(6,11, true);
-//        lines[3] = new Line(20,1, true);
-//        lines[4] = new Line(3,4, true);
-//        lines[5] = new Line(9,20,false);
-//        lines[6] = new Line(21,6,false);
-//        lines[7] = new Line(12,24,false);
-//        lines[8] = new Line(10,24,false);
-//        lines[9] = new Line(20,16,false);
-        for(Line l : lines)
-        {
-            System.out.println(l);
-        }
-        ArrayList<Point> bruteAnswer = new BruteForce().solve(lines);  
-        System.out.print("Length: "+bruteAnswer.size()+"\n");
-        for(Point p : bruteAnswer)
-        {
-            printPoint(p);
-        }
+    	long bruteStart;
+		long bruteEnd;
+		long smartStart;
+		long smartEnd;
+    	
+        Line[] lines = LineGenerator.generateLines(10, 100);
+        Line[] lines1 = LineGenerator.generateLines(100, 100);
+        Line[] lines2 = LineGenerator.generateLines(500, 100);
+        Line[] lines3 = LineGenerator.generateLines(1000, 100);
+        Line[] lines4 = LineGenerator.generateLines(2000, 100);
+        Line[] lines5 = LineGenerator.generateLines(10000, 100);
+        Line[] lines6 = LineGenerator.generateLines(20000, 100);
         
-        ArrayList<Point> sweepAnswer = new SweepLine().solve(lines);
-        System.out.print("Length: "+sweepAnswer.size()+"\n");
-        for(Point p : sweepAnswer)
-        {
-            printPoint(p);
-        }
+        //10 lines
+        bruteStart = System.nanoTime();
+        ArrayList<Point> bruteAnswer = new BruteForce().solve(lines); 
+		bruteEnd = System.nanoTime();
+		System.out.println("Brute force ran in: " + (bruteEnd - bruteStart) + " nanoseconds for 10 lines");
+   
+		smartStart = System.nanoTime();
+		ArrayList<Point> sweepAnswer = new SweepLine().solve(lines);
+		smartEnd = System.nanoTime();
+		System.out.println("Smart implementation ran in: " + (smartEnd - smartStart) + " nanoseconds for 10 lines");
+       
+		//100 lines
+        bruteStart = System.nanoTime();
+        ArrayList<Point> bruteAnswer1 = new BruteForce().solve(lines1); 
+		bruteEnd = System.nanoTime();
+		System.out.println("Brute force ran in: " + (bruteEnd - bruteStart) + " nanoseconds for 100 lines");
+   
+		smartStart = System.nanoTime();
+		ArrayList<Point> sweepAnswer1 = new SweepLine().solve(lines1);
+		smartEnd = System.nanoTime();
+		System.out.println("Smart implementation ran in: " + (smartEnd - smartStart) + " nanoseconds for 100 lines");
+		
+		//500 lines
+		bruteStart = System.nanoTime();
+        ArrayList<Point> bruteAnswer2 = new BruteForce().solve(lines2); 
+		bruteEnd = System.nanoTime();
+		System.out.println("Brute force ran in: " + (bruteEnd - bruteStart) + " nanoseconds for 500 lines");
+   
+		smartStart = System.nanoTime();
+		ArrayList<Point> sweepAnswer2 = new SweepLine().solve(lines2);
+		smartEnd = System.nanoTime();
+		System.out.println("Smart implementation ran in: " + (smartEnd - smartStart) + " nanoseconds for 500 lines");
+		
+		//1000 lines
+		bruteStart = System.nanoTime();
+        ArrayList<Point> bruteAnswer3 = new BruteForce().solve(lines3); 
+		bruteEnd = System.nanoTime();
+		System.out.println("Brute force ran in: " + (bruteEnd - bruteStart) + " nanoseconds for 1000 lines");
+   
+		smartStart = System.nanoTime();
+		ArrayList<Point> sweepAnswer3 = new SweepLine().solve(lines3);
+		smartEnd = System.nanoTime();
+		System.out.println("Smart implementation ran in: " + (smartEnd - smartStart) + " nanoseconds for 1000 lines");
+		
+		//2000 lines
+		bruteStart = System.nanoTime();
+        ArrayList<Point> bruteAnswer4 = new BruteForce().solve(lines4); 
+		bruteEnd = System.nanoTime();
+		System.out.println("Brute force ran in: " + (bruteEnd - bruteStart) + " nanoseconds for 2000 lines");
+   
+		smartStart = System.nanoTime();
+		ArrayList<Point> sweepAnswer4 = new SweepLine().solve(lines4);
+		smartEnd = System.nanoTime();
+		System.out.println("Smart implementation ran in: " + (smartEnd - smartStart) + " nanoseconds for 2000 lines");
+		
+		//10000 lines
+		bruteStart = System.nanoTime();
+        ArrayList<Point> bruteAnswer5 = new BruteForce().solve(lines5); 
+		bruteEnd = System.nanoTime();
+		System.out.println("Brute force ran in: " + (bruteEnd - bruteStart) + " nanoseconds for 10000 lines");
+   
+		smartStart = System.nanoTime();
+		ArrayList<Point> sweepAnswer5 = new SweepLine().solve(lines5);
+		smartEnd = System.nanoTime();
+		System.out.println("Smart implementation ran in: " + (smartEnd - smartStart) + " nanoseconds for 10000 lines");
+		
+		//20000 lines
+		bruteStart = System.nanoTime();
+        ArrayList<Point> bruteAnswer6 = new BruteForce().solve(lines6); 
+		bruteEnd = System.nanoTime();
+		System.out.println("Brute force ran in: " + (bruteEnd - bruteStart) + " nanoseconds for 20000 lines");
+   
+		smartStart = System.nanoTime();
+		ArrayList<Point> sweepAnswer6 = new SweepLine().solve(lines6);
+		smartEnd = System.nanoTime();
+		System.out.println("Smart implementation ran in: " + (smartEnd - smartStart) + " nanoseconds for 20000 lines");
+    
     }
     
     private static void printPoint(Point p)
